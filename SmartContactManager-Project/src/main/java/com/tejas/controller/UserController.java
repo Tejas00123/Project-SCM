b6fd8f216adc41fdaf3192dbc574d365e8a48566
Tejas Gavali <tejasgavali8280@gmail.com>
2024-10-10 12:19:59 +0530

    just defining routes for user and added spring security starter to see its default behaviour....1

---
+++ SmartContactManager-Project/src/main/java/com/tejas/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.tejas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+	@GetMapping("/profile")
+	public String showProfilePage() {
+		return "user/profile";
+	}
+	
+	@GetMapping("/dashboard")
+	public String showDashboardPage() {
+		return "user/dashboard";
+	}
+	
+}
