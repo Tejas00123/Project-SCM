058e66c18d1aec06206c935663136b60b655c827
Tejas Gavali <tejasgavali8280@gmail.com>
2024-10-20 21:47:56 +0530

    Some pendig work completed....

--- SmartContactManager-Project/src/main/java/com/tejas/services/IEmailMgmtService.java
+++ SmartContactManager-Project/src/main/java/com/tejas/services/IEmailMgmtService.java
@@ -3,4 +3,10 @@ package com.tejas.services;
 public interface IEmailMgmtService {
 
 	public void sendEmail(String to, String subject, String body);
+	
+	//below 2 method are for feedback and direct message we using
+	//verication controller for below method
+	public void feedBackEmail(String name,String email,String message);
+	
+	public void directMailToContact(String name,String to,String message);
 }
